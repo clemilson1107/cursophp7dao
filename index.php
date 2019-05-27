a43b76c9e6abd7a564c1e8f169117d757a402f4b
Clemilson <clemilson.ferreira_@hotmail.com>
2019-05-27 20:45:22 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,27 @@
 
 require_once("config.php");
 
-$root = new Usuarios();
-$root->loadById(2);
+//carrega um usuario
+//$root = new Usuarios();
+//$root->loadById(2);
 
-echo $root;
+//echo $root;
+
+//carrega uma lista de usuario
+
+//$lista = Usuarios::getList();
+//echo json_encode($lista);
+
+
+//carrea uma lista de usuarios, buscando pelo login
+//$search = Usuarios::search("e");
+
+//echo json_encode($search);
+
+//carrega um usuario usando login e senha
+$usuario = new Usuarios();
+$usuario->login("clemilson1107", "30231112");
+
+echo $usuario;
 
  ?>
